0ecd2f3a629deec0bc65419cbb8d2be83fba08c3
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-05-18 14:43:13 +0900

    Scriptsファイル内の整理, Message関連クラスの修正/追加

---
+++ GameCreate/Assets/Scripts/Message/MessageManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour
+{
+    [SerializeField] private List<MessagePresenter> _messagePresentersLists = new List<MessagePresenter>();
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _messagePresentersLists[(int)CommonParam.TextType.MainText].
+                SetMessage(CommonParam.TextType.MainText, "お試しメッセージ", () => Debug.Log("メッセージ終了"));
+        }
+    }
+}
--- GameCreate/Assets/Scripts/Message/MessagePresenter.cs
+++ GameCreate/Assets/Scripts/Message/MessagePresenter.cs
@@ -7,15 +7,46 @@ using UnityEngine;
 public class MessagePresenter : MonoBehaviour
 {
     [SerializeField] private float _messageSpeed = 0.5f;
+    public float MessageSpeed { set { _messageSpeed = value; } }
+    private MessageView _textView;
+    private CommonParam.TextType _textType = CommonParam.TextType.MainText;
 
+    private string _originalMessage = "";
+    private string _dispMessage = "";
+    private int _messageCount = 0;
+    private float _timer = 0;
+    private System.Action _callback = null;
+
+    public void SetMessage(CommonParam.TextType textType, string message, System.Action callback = null)
+    {
+        _callback = callback;
+        _textType = textType;
+        _originalMessage = message;
+        _dispMessage = "";
+        _messageCount = 0;
+        _timer = 0;
+        StartCoroutine(MessageDisp());
+    }
     void Start()
     {
-        
+        _textView = GetComponent<MessageView>();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MessageDisp()
     {
-        
+        while(_messageCount < _originalMessage.Length)
+        {
+            _timer += Time.deltaTime;
+            if(_timer >= _messageSpeed)
+            {
+                _timer = 0;
+                _messageCount++;
+                _dispMessage = _originalMessage.Substring(0, _messageCount);
+                _textView.SetMessage(_dispMessage);
+            }
+            yield return null;
+        }
+        if (_callback != null) _callback();
     }
+
 }
