c5366153e5e69ee963a512ee0b46c119c5f49110
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-11-24 18:15:26 +0900

    ScriptableObjectの使用テストフォルダ追加

---
+++ GameCreate/Assets/ScriptableObject/Scripts/EnemyData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SetData/Create EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public string enemyName;
+    public int maxHp;
+    public int atk;
+    public int def;
+    public int exp;
+    public int gold;
+}
---
+++ GameCreate/Assets/ScriptableObject/Scripts/EnemyDataSet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyDataSet : MonoBehaviour
+{
+    [SerializeField] private EnemyData data;
+    [SerializeField] private Text _text;
+
+    void Start()
+    {
+        _text.text =
+                    "名前：　" + data.enemyName + "\n"
+                  + "体力：　" + data.maxHp + "\n"
+                  + "攻撃：　" + data.atk + "\n"
+                  + "防御：　" + data.def + "\n"
+                  + "経験：　" + data.def + "\n".ToString();
+    }
+
+}
