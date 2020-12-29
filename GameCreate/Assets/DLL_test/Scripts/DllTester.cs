537ffcddb7278e0dc7f6cb565d52e7e2930afe3d
Teppei_Niitsuma <niitsuma.0910.tetsu@icloud.com>
2020-12-29 22:28:30 +0900

    C++dllの読み込みテスト

---
+++ GameCreate/Assets/DLL_test/Scripts/DllTester.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+static class DLL
+{
+    [DllImport("DLLtest", CallingConvention = CallingConvention.StdCall)]
+    public static extern int Add_function(int a, int b);
+}
+public class DllTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(DLL.Add_function(3, 4));
+    }
+
+}
